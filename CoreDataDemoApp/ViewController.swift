defae3aa62b0b9f45f003b71918a06a0124ef934
A.Vassel <102523970+AVassel@users.noreply.github.com>
2022-12-06 09:14:37 +0300

    Initial Commit

---
+++ CoreDataDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDemoApp
+//
+//  Created by Anton Vassel on 06.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
